700dc04adfa7fe8cc9bc5fc275c006f45b5a6b98
James Mansour <jamestmansour@outlook.com>
2021-04-26 21:51:18 -0700

    Adds counter to the add task screen when clicking the add task button

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # taskmaster
+
+Taskmaster is an application used to track you todos and help enable/ motivate you to get them done!
+
+## recent changes
+
+v 0.1
+* Adds the landing screen with links to Add Task and All tasks.
+* Adds create new task page with a submit button that increments task count.
+* Adds all Tasks page that is a place holder for future content.
+
+![image description](screenshots/homepage.PNG)
--- app/src/main/java/com/f0rgiv/taskmaster/activities/AddTask.java
+++ app/src/main/java/com/f0rgiv/taskmaster/activities/AddTask.java
@@ -3,14 +3,28 @@ package com.f0rgiv.taskmaster.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import com.f0rgiv.taskmaster.R;
 
-public class AddTask extends AppCompatActivity {
+import java.util.Locale;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_task);
-    }
+public class AddTask extends AppCompatActivity {
+  String TAG = "AddTask";
+
+  Integer count = 0;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_add_task);
+
+
+    findViewById(R.id.createNewTask).setOnClickListener(view -> {
+      count += 1;
+      ((TextView) findViewById(R.id.submitMsgText)).setText(R.string.submittedMsg);
+      ((TextView) findViewById(R.id.totalTasksText)).setText(String.format(Locale.ENGLISH,"Total tasks: %d", count));
+    });
+  }
 }
