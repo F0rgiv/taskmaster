530d023bcd1b344f8f5e6bb48f34483d08642cb4
James Mansour <jamestmansour@outlook.com>
2021-04-27 16:32:25 -0700

    Adds ability to use the menu to navigate to settings and moves data population to onResume.

--- app/src/main/java/com/f0rgiv/taskmaster/activities/MainActivity.java
+++ app/src/main/java/com/f0rgiv/taskmaster/activities/MainActivity.java
@@ -6,6 +6,9 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -22,18 +25,27 @@ public class MainActivity extends AppCompatActivity {
   String TAG = "main";
 
   @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    MenuInflater inflater = getMenuInflater();
+    inflater.inflate(R.menu.settings, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+      case R.id.go_to_settings:
+        MainActivity.this.startActivity(new Intent(MainActivity.this, Settings.class));
+        return true;
+      default:
+        return true;
+    }
+  }
+
+  @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
-
-    //data population
-
-    String greeting = "Tasks";
-    String username = preferences.getString("username", null);
-    if (username != null) greeting = String.format(Locale.ENGLISH, "%s's tasks", username);
-    ((TextView) findViewById(R.id.mainTasksGreetingLabel)).setText(greeting);
-
     //onClickListeners
 
     findViewById(R.id.addTaskButton).setOnClickListener(view ->
@@ -42,17 +54,23 @@ public class MainActivity extends AppCompatActivity {
     findViewById(R.id.allTasks).setOnClickListener(view ->
       MainActivity.this.startActivity(new Intent(MainActivity.this, AllTasks.class)));
 
-    findViewById(R.id.mainGoToSettingsButton).setOnClickListener(view ->
-      MainActivity.this.startActivity(new Intent(MainActivity.this, Settings.class)));
-
     findViewById(R.id.task1button).setOnClickListener(HandleTaskButtonClick());
     findViewById(R.id.task2button).setOnClickListener(HandleTaskButtonClick());
     findViewById(R.id.task3button).setOnClickListener(HandleTaskButtonClick());
     findViewById(R.id.task4button).setOnClickListener(HandleTaskButtonClick());
   }
 
-  //onClick callbacks
+  @Override
+  protected void onResume() {
+    super.onResume();
+    SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+    String greeting = "Tasks";
+    String username = preferences.getString("username", null);
+    if (username != null) greeting = String.format(Locale.ENGLISH, "%s's tasks", username);
+    ((TextView) findViewById(R.id.mainTasksGreetingLabel)).setText(greeting);
+  }
 
+  //onClick callbacks
   private View.OnClickListener HandleTaskButtonClick() {
     return view ->{
       Intent intent = new Intent(MainActivity.this, TaskDetailActivity.class);
