80ebf15758ee368d9557b33716f020cb6389deac
James Mansour <jamestmansour@outlook.com>
2021-05-04 21:46:16 -0700

    Updates task so id matches CloudTask

--- app/src/main/java/com/f0rgiv/taskmaster/models/Task.java
+++ app/src/main/java/com/f0rgiv/taskmaster/models/Task.java
@@ -1,20 +1,38 @@
 package com.f0rgiv.taskmaster.models;
 
+import androidx.annotation.NonNull;
 import androidx.room.Entity;
+import androidx.room.Ignore;
 import androidx.room.PrimaryKey;
 
 @Entity
 public class Task {
-  @PrimaryKey(autoGenerate = true)
-  public long id;
+  @PrimaryKey()
+  @NonNull
+  public String id;
 
   public String title;
   public String description;
   public String state;
 
+  @Ignore
   public Task(String title, String description, String state) {
     this.title = title;
     this.description = description;
     this.state = state;
+  }
+
+
+  public Task(String id, String title, String description, String state) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.state = state;
+  }
+
+  @NonNull
+  @Override
+  public String toString() {
+    return this.title;
   }
 }
